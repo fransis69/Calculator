d2b4a38295677e59204d152d11fa91b6990afc62
fransis69 <fransisfransis77@gmail.com>
2025-03-30 11:28:19 +0530

    Added Addition operation

---
+++ calculator.py
@@ -0,0 +1,9 @@
+a = int(input("Enter a Value: "))
+
+b = int(input("Enter another Value: "))
+
+opearation = input("Enter Opeartion ex: (a+b): ")
+# Addition 
+
+if opearation == 'a+b':
+    print(a+b)
