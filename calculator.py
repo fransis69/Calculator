4d7b1434f32db608a0c68c92d30725d8d156a4e7
fransis69 <fransisfransis77@gmail.com>
2025-03-31 12:27:58 +0530

    Updted

--- calculator.py
+++ calculator.py
@@ -1,9 +1,1 @@
-a = int(input("Enter a Value: "))
-
-b = int(input("Enter another Value: "))
-
-opearation = input("Enter Opeartion ex: (a+b): ")
-# Addition 
-
-if opearation == 'a+b':
-    print(a+b)
+print("Hello, World")
